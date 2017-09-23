90e5437a7d2129cad45491b007947d252c32d0d3
Adam Butler <adam.j.butler@gmail.com>
2017-09-23 23:16:26 +0100

    Fix virtualbox guest additions installation
    Remove pre-installed guest additions
    Install x11 additions

--- script/update.sh
+++ script/update.sh
@@ -5,5 +5,8 @@ apt="apt-get -qq -y"
 echo "==> Updating list of repositories"
 apt-get -y update
 
+# Remove any pre-installed virtualbox packages
+apt-get -y purge virtualbox*
+
 echo "==> Performing dist-upgrade (all packages and kernel)"
 apt-get -y dist-upgrade
--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -4,11 +4,11 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
     # Assume that we've installed all the prerequisites:
     # kernel-headers-$(uname -r) kernel-devel-$(uname -r) gcc make perl
-    # from the install media via ks.cfg
+    # from the install media
 
     VBOX_VERSION=$(cat ~/.vbox_version)
     mount -o loop ~/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-    sh /mnt/VBoxLinuxAdditions.run --nox11
+    sh /mnt/VBoxLinuxAdditions.run
     umount /mnt
     rm -rf ~/VBoxGuestAdditions_$VBOX_VERSION.iso
     rm -f ~/.vbox_version
